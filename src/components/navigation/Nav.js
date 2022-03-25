06a356729aa8aa27390dad2dbca865c61fb7cc0d
ehdeneve <elwyn_de_neve@hotmail.com>
2022-03-25 14:36:27 +0100

    leeg document

---
+++ src/components/navigation/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav({children}) {
+    return (
+        <div >
+            {children}
+        </div>
+    );
+}
+
+export default Nav;
