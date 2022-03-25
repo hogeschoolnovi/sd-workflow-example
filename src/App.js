08f883ee7091e8a4a640c17388e34107f42edd24
Albatroaz7 <sambarnhoorn@gmail.com>
2022-03-25 14:36:33 +0100

    added the page components to their correct paths in App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import './App.css';
 import {Switch, Route} from "react-router-dom";
+import About from "./pages/about/About";
+import Home from "./pages/home/Home";
+import Services from "./pages/services/Services";
+import Team from "./pages/team/Team";
+import Contact from "./pages/contact/Contact";
 
 function App() {
   return (
     <div>
       <Switch>
           <Route exact path='/'>
-
+              <Home/>
           </Route>
           <Route path="/about-us">
-
+              <About/>
           </Route>
           <Route path="/services">
-
+              <Services/>
           </Route>
           <Route path="/team">
-
+              <Team/>
           </Route>
           <Route path="/contact">
-
+              <Contact/>
           </Route>
-
       </Switch>
     </div>
   );
